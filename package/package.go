426f1d1ed3739fcfc0ae6ef625f9b40eebe373be
Jurgen <jurgen@virtuos.com.cn>
2017-11-28 12:52:52 +0800

    *: add xbase as dependency package

--- package/package.go
+++ package/package.go
@@ -1,6 +1,7 @@
 package xbinmap
 
 import (
+	"github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xcode/denv"
 	"github.com/jurgen-kluft/xentry/package"
 	"github.com/jurgen-kluft/xunittest/package"
@@ -11,20 +12,23 @@ func GetPackage() *denv.Package {
 	// Dependencies
 	unittestpkg := xunittest.GetPackage()
 	entrypkg := xentry.GetPackage()
+	basepkg := xbase.GetPackage()
 
 	// The main (xbinmap) package
 	mainpkg := denv.NewPackage("xbinmap")
 	mainpkg.AddPackage(unittestpkg)
 	mainpkg.AddPackage(entrypkg)
+	mainpkg.AddPackage(basepkg)
 
 	// 'xbinmap' library
 	mainlib := denv.SetupDefaultCppLibProject("xbinmap", "github.com\\jurgen-kluft\\xbinmap")
-	mainlib.Dependencies = append(mainlib.Dependencies, unittestpkg.GetMainLib())
+	mainlib.Dependencies = append(mainlib.Dependencies, basepkg.GetMainLib())
 
 	// 'xbinmap' unittest project
 	maintest := denv.SetupDefaultCppTestProject("xbinmap_test", "github.com\\jurgen-kluft\\xbinmap")
 	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, entrypkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, basepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
